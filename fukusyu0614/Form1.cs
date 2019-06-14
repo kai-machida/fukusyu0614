f6e2d7a555ec71da1a0fc54a40948a00f5f62cca
kai-machida <49702001+kai-machida@users.noreply.github.com>
2019-06-14 13:27:27 +0900

    跳ね返り完成、タイマーを停止未完成

--- fukusyu0614/Form1.cs
+++ fukusyu0614/Form1.cs
@@ -12,9 +12,38 @@ namespace fukusyu0614
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = -vx;
+            }
+
+            if (label1.Right >288)
+            {
+                vx = -vx;
+            }
+
+            if (label1.Top < 0)
+            {
+                vy = -vy;
+            }
+
+            if (label1.Bottom > 250)
+            {
+                vy = -vy;
+            }
+
         }
     }
 }
